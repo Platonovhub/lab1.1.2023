31c5f230e9efdcc7f16eab021768fa479b5f3d25
Platonovhub <112153182+Platonovhub@users.noreply.github.com>
2023-03-16 16:42:27 +0200

    Add files via upload

---
+++ 1.2.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace OOL_LB_1._3
+{
+    delegate float Avg(int x,int y ,int z);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Avg avg = Average;
+            float Average(int x, int y, int z) => (x + y + z) / 3;
+            float result = avg(x, y, z);
+            Console.WriteLine("-----------");
+            Console.WriteLine(result);
+        }
+    }
+}
